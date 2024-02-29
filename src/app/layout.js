b7474bd7b49b9b8ead371a7fa38d3f0499b92d49
Rishi <rishilahoti99@gmail.com>
2024-02-29 15:35:20 +0530

    google analytic

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
 import Header from '../components/Header';
+import { Head } from "next/document";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -12,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="P2_1zWxkE0R-QmgUGw4dGpmHqVlIO0X-SaoFDdH-ciM" />
+      </Head>
       <body className={inter.className}>
         <Header />
         {children}
