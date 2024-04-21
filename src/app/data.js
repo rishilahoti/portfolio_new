eb9e9fa68630b90b25ac58bb8d1bc1173c19d593
Rishi <rishilahoti99@gmail.com>
2024-04-21 15:13:59 +0530

    project section updated

--- src/app/data.js
+++ src/app/data.js
@@ -1,37 +1,46 @@
 export const projects = [
   {
-    title: "test",
-    description: "test description",
-    src: "rock.jpg",
-    link: "",
+    title: "Portfolio",
+    description: `Spearheaded the development of a professional portfolio website, drawing inspiration from the renowned portfolio of Dennis Snellenberg.`,
+    description2: `Leveraged NexJs, Sass, JavaScript, CSS, and HTML to create a dynamic and visually appealing user experience.`,
+    description3: `Ensured full responsiveness across various devices for optimal user engagement.`,
+    src: "portfolio_new.png",
+    link: "https://github.com/rishilahoti/portfolio_new",
     color: "#ffc6a6"
   },
   {
-    title: "test",
-    description: "test description",
-    src: "tree.jpg",
-    link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
+    title: "Vanilla Portfolio",
+    description: `Conceptualized and implemented a captivating portfolio showcasing a range of projects through animated cards and a skill graph.`,
+    description2: `Integrated contact support functionality and seamlessly integrated social media handles.`,
+    description3: `Achieved full responsiveness to guarantee an exceptional user experience on all devices.`,
+    src: "vanilla.png",
+    link: "https://github.com/rishilahoti/Portfolio",
     color: "#e5e0ff"
   },
   {
-    title: "test",
-    description: "test description",
-    src: "water.jpg",
-    link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
+    title: "PESU Food Cart",
+    description: `Design and development efforts for a dynamic food ordering web application tailored for the college cafeteria.`,
+    description2: `Implementing user-friendly interfaces and streamlined ordering processes to enhance user experience and satisfaction.`,
+    description3: `Using modern web development technologies and best practices to deliver a high-quality, responsive, and scalable solution.`,
+    src: "foodapp.png",
+    link: "https://www.figma.com/file/sbPGg7Yz9J2dJvGEHYJywa/PESU-Food-Cart?type=design&node-id=0%3A1&mode=design&t=69Fq8cPhfENvQmhR-1",
     color: "#e1f0da"
   },
   {
-    title: "test",
-    description: "test description",
-    src: "house.jpg",
-    link: "https://www.ignant.com/2019/03/13/a-photographic-series-depicting-the-uncertain-future-of-denmarks-treasured-coastlines/",
+    title: "SOR-T Project",
+    description: `Led the design process for the SOR-T project, envisioning the user interface and experience for an imaginary Potential Building Agency.`,
+    description2: `Utilized Figma to create visually stunning and intuitive designs, informed by the principles of Crazy-8.`,
+    description3: `Ensured coherence and user-friendliness throughout the interface for maximum usability.`,
+    src: "sort.png",
+    link: "https://www.figma.com/file/HZeeFTl4U8AcxwgAMPjU60/SORT-(Community)?type=design&mode=design&t=SwEQuB175Ol0zc6K-1",
     color: "#ffe3e1"
   },
   {
-    title: "test",
-    description: "test description",
-    src: "cactus.jpg",
-    link: "https://www.ignant.com/2023/04/12/mark-rammers-all-over-again-is-a-study-of-regret-and-the-willingness-to-move-forward/",
+    title: "React Movie App",
+    description: `Recreated a movie search application using React, integrating real-time data retrieval from the OMDB API.`,
+    description2: `Implemented features allowing users to add and remove movies from favorites, enhancing personalization and user engagement.`,
+    src: "movieapp.png",
+    link: "https://github.com/rishilahoti/React-Movie-App",
     color: "#e3f4f4"
   }
 ]
